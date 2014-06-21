083cdf073816ab851d600f0506c1da8f5b5970f5
hly <huangluyang007@163.com>
2014-06-21 22:54:53 +0800

    init

---
+++ MyCaiCai/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyCaiCai
+//
+//  Created by huangluyang on 14-6-21.
+//  Copyright (c) 2014年 huangluyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MLAppDelegate class]));
+    }
+}
